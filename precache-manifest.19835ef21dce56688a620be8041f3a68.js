dbd2907158b6a7d03fcd1d84fca5675b7255cd4c
maknetaRo <makneta@gmail.com>
2020-08-02 01:34:26 +0200

    Updates

---
+++ precache-manifest.19835ef21dce56688a620be8041f3a68.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45e72da292eb8e5d101805615e6b52f",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "46609fb6d2a136e9149f",
+    "url": "/quote-machine/static/css/main.2340d079.chunk.css"
+  },
+  {
+    "revision": "796b59d2d45e2c26d2f1",
+    "url": "/quote-machine/static/js/2.3b0b9ceb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.3b0b9ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46609fb6d2a136e9149f",
+    "url": "/quote-machine/static/js/main.f05b9d47.chunk.js"
+  },
+  {
+    "revision": "a204d0198decee076aa5",
+    "url": "/quote-machine/static/js/runtime-main.e62646e6.js"
+  },
+  {
+    "revision": "36e3605a0cef065fa4e96ae68ac0b740",
+    "url": "/quote-machine/static/media/Arystoteles.36e3605a.jfif"
+  },
+  {
+    "revision": "8ee2cad4b584148b74c7a83a15129941",
+    "url": "/quote-machine/static/media/Benjamin-Franklin.8ee2cad4.jfif"
+  },
+  {
+    "revision": "2ea84f05fef5e8a38faf065e0a1e57aa",
+    "url": "/quote-machine/static/media/Eleanor-Roosevelt.2ea84f05.jfif"
+  },
+  {
+    "revision": "fd57c25c83b5426b0441c3b3cc773327",
+    "url": "/quote-machine/static/media/James-Cameron-2012.fd57c25c.jpg"
+  },
+  {
+    "revision": "fbd204978f795155b56f06cd79ea8e3b",
+    "url": "/quote-machine/static/media/Nelson-Mandela.fbd20497.jpg"
+  },
+  {
+    "revision": "af2400dd1e0d8d6882f61144a789e948",
+    "url": "/quote-machine/static/media/Walt_Disney_1946.af2400dd.JPG"
+  }
+]);
